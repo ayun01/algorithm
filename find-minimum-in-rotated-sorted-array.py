e899fb247dafc348cf7b8a16c5e524c6723c7790
ayun01 <33712067+ayun01@users.noreply.github.com>
2017-12-31 15:12:54 -0800

    Create find-minimum-in-rotated-sorted-array.py

---
+++ find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param: nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+    def findMin(self, nums):
+        if nums is None or len(nums) <= 0:
+            return None
+        
+        l,r = 0, len(nums) - 1
+        
+        target = nums[r]
+        
+        while l + 1 < r:
+            m = l + (r-l)/2
+            # find the first elem that <= target
+            if target >= nums[m]:
+                r = m
+            else:
+                l = m
+        
+        if nums[l] <= nums[r]:
+            return nums[l]
+        else:
+            return nums[r]
+            
